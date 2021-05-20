aa65a184267087c9f83a23a80661cbe636939c3e
Hobbes <mxhobbes@gmail.com>
2021-05-20 21:59:11 +0000

    Completed step 3, part of step 4.

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,18 +1,67 @@
 "use strict";
-const taradingCardData = [
+const tradingCardData = [
   {
     name: 'Balloonicorn',
     skill: 'video games',
     imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpg'
+  },
+
+  {
+    name: 'Seed.py',
+    skill: 'making curry dishes',
+    imgUrl: '/static/img/seedpy.jpg'
+  },
+
+  {
+    name: 'Polymorphism',
+    skill: 'costumes',
+    imgUrl: '/static/img/polymorphism.jpg'
+  },
+
+  {
+    name: 'Short Stack Overflow',
+    skill: 'ocean animal trivia',
+    imgUrl: '/static/img/shortstack-overflow.jpg'
+  },
+
+  {
+    name: 'Merge',
+    skill: 'bullet journaling',
+    imgUrl: '/static/img/merge.jpg'
   }
-]
+];
+
+
+//want to see what is in tradingcarddata
+
+console.log(tradingCardData);
+
 
 function TradingCard(props) {
   return (
     <div className="card">
       <h2>Name: {props.name}</h2>
       <img src={props.imgUrl} />
-      <h2>Skill: </h2>
+      <h2>Skill:{props.skill} </h2>
     </div>
   );
 }
@@ -49,3 +98,26 @@ ReactDOM.render(
   ),
   document.querySelector('#llambda')
 );
+
+ReactDOM.render(
+  (
+    <TradingCard
+      name="Seedpy"
+      skill="puddle jumping"
+      imgUrl="/static/img/seedpy.jpeg"
+    />
+  ),
+  document.querySelector('#seedpy')
+);
+
+ReactDOM.render(
+  (
+    <TradingCard
+      name="Polymorphism"
+      skill="sun bathing"
+      imgUrl="/static/img/polymorphism.jpeg"
+    />
+  ),
+  document.querySelector('#polymorphism')
+);
+
